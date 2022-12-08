48fd2b87b259c118f74f43cc121ba51db16cd39e
namtt <ttnam886@gmail.com>
2022-12-08 09:15:59 +0700

    add lamdba command injection

---
+++ Lambda Command Injection/README.md
@@ -0,0 +1,4 @@
+# Terraform-AWS-API-Gateway-and-Lambda
+
+## Tutorial
+[AWS: API Gateway invoking Lambda function with Terraform](https://www.bogotobogo.com/DevOps/AWS/aws-API-Gateway-Lambda-Terraform.php)
---
+++ Lambda Command Injection/lambda.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+   region = "us-east-1"
+}
+
+data "archive_file" "my_lambda_function" {
+  source_dir  = "${path.module}/examplepy/"
+  output_path = "${path.module}/examplepy.zip"
+  type        = "zip"
+}
+
+resource "aws_lambda_function" "examplepy" {
+   function_name = "Serverlessexamplepy"
+
+   # The S3 bucket should already exists
+   filename = "examplepy.zip"
+   source_code_hash = data.archive_file.my_lambda_function.output_base64sha256
+ 
+
+   # "lambda_function" is the filename within the zip file (lambda_function.py) 
+   # and "handler" is the name of the property 
+   # under which the handler function was exported in that file.
+   handler = "lambda_function.lambda_handler"
+   runtime = "python3.8"
+
+   role = aws_iam_role.lambda_execpy.arn
+}
+
+ # IAM role which dictates what other AWS services the Lambda function may access.
+resource "aws_iam_role" "lambda_execpy" {
+   name = "serverless_example_lambdapy"
+
+   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_lambda_permission" "apigw" {
+   statement_id  = "AllowAPIGatewayInvoke"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.examplepy.function_name
+   principal     = "apigateway.amazonaws.com"
+
+   # The "/*/*" portion grants access from any method on any resource
+   # within the API Gateway REST API.
+   source_arn = "${aws_api_gateway_rest_api.examplepy.execution_arn}/*/*"
+}
